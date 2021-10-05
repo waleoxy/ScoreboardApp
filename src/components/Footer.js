423d3761ae7826ddb18a3e8fb6990ad12d1e15eb
Olawale Otubu <waleoxy@gmail.com>
2021-10-05 08:01:57 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,19 @@ import "./Footer.css"
 function Footer() {
     return (
      <div className="footer">
-
-     </div>
+        <div className="container py-3">
+                <div className="row">
+                    <div className="col-md-6">
+                        <p className="text text-capitalize">
+                            copyright &copy; Olawale Otubu
+                          {new Date().getFullYear()}
+                            {" "}all rights reserved{""}
+                        </p>
+                    </div>
+                      <div className="col-md=6  d-flex justify-content-between"></div> 
+                </div>
+            </div>
+            </div>
     )
 }
 
